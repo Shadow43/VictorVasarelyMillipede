0a85a40647b6e95f64388fbf2a408d86a4cd5912
Nathan Owens <ShadowFox_43@hotmail.com>
2019-02-16 17:21:16 +1000

    Second commit - game prototype - bullet not firing - enemy not moving - help needed for both

---
+++ Assets/Scripts/Player/BulletForce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletForce : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float ballForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            {
+                rb.isKinematic = false;
+                rb.AddForce(new Vector2(ballForce, ballForce));
+                transform.SetParent(null);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // movement code
+    public Rigidbody2D rb;
+    public float speed;
+    public float x;
+    public float y;
+    // clamping code
+    public float XMaxMin = -2.71f;
+    public float XMaxMax = 2.71f;
+    public float YMaxMin = -6.76f;
+    public float YMaxMax = -5.76f;
+    // buttet spawn code
+    public GameObject prefab;
+    public Transform spawnAt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Mouse X");
+        y = Input.GetAxis("Mouse Y");
+        Vector3 pos = (transform.position);
+        pos.x = Mathf.Clamp(pos.x, XMaxMin, XMaxMax);
+        pos.y = Mathf.Clamp(pos.y, YMaxMin, YMaxMax);
+        transform.position = pos;
+        rb.velocity = new Vector2(x, y) * speed;
+
+    }
+}
